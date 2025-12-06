cbc72032d49888a6878f74ecb301f8e737c3b191
ignatukdmitrij228 <ignatukdmitrij228@gmail.com>
2025-12-07 00:15:27 +0300

    Update rot.cpp

--- rot.cpp
+++ rot.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+using namespace std; // чтобы не писать std::
 int main() {
     // input array
     int arr[] = {1, 2, 3, 4, 5};
@@ -8,16 +8,16 @@ int main() {
     int left = 0;
     int right = n - 1;
 
-    while (left < right {
+    while (left < right) { // добавил закрывающую скобку в while 
         int tmp = arr[left];
         arr[left] = arr[right];
         arr[right] = tmp;
         left++;
-        right-;
+        right--; добавил - т.к это декремент
     }
 
     // output
-    for (int i == 0; i < n; i++) {
+    for (int i = 0; i < n; i++) { // убрал = т.к при инициализаци используется унарный оператор присваивания =
         cout << arr[i] << " ";
     }
     cout << endl;
